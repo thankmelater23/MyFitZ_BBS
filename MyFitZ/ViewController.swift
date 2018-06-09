09ce2864f23933861e65e2d2de248b0de305067d
Andre Villanueva <Andre@AndreVillanueva.com>
2018-06-09 01:07:10 -0700

    Initial Commit

---
+++ MyFitZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFitZ
+//
+//  Created by Andre on 6/9/18.
+//  Copyright © 2018 Bang Bang Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
