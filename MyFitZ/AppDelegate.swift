b81ab7e1b2962170df93d34609029b0a29d6a441
Andre Villanueva <Andre@AndreVillanueva.com>
2018-06-09 04:37:52 -0700

    Fabric setup

--- MyFitZ/AppDelegate.swift
+++ MyFitZ/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    Fabric.with([Crashlytics.self, Answers.self])
+
     // Override point for customization after application launch.
     return true
   }
